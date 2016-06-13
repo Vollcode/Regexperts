6a055091bde1c096678e7150a93561efbadb5809
Emmett Walsh <emmett.finbarr.walsh@gmail.com>
2016-06-13 17:24:31 +0100

    game controller redifined to use regexp :hankey:

--- app/js/controllers/GameController.js
+++ app/js/controllers/GameController.js
@@ -2,17 +2,17 @@ angular.module('regexpert')
        .controller('GameController',[ function(){
 
   var vm = this;
-  
+
   vm.isWon = isWon;
-  vm.expectation = "good input";
-
-
+  vm.expectation = "HPF";
+  vm.levelText = 'Horse brHeeding is reproduction in horses, and particularly the human-directed process of selective breeding of animals, particularly purebred horses of a given breed. Planned matings can be used to produce specifically desired characteristics in domesticated horses. Furthermore, modern breeding management and technologies can increase the rate of conception, a healthy pregnancy, and successful foaling.';
 
   function isWon(input) {
-    return input === vm.expectation;
+    var regexp = new RegExp(input, 'g');
+    var matchedValue = vm.levelText.match(regexp);
+    var matchFound = matchedValue === null ? [] : matchedValue;
+    return matchFound.join('') === vm.expectation;
   }
-
-
 
 
 }]);
--- test/unit/GameController.spec.js
+++ test/unit/GameController.spec.js
@@ -1,15 +1,25 @@
 describe('GameController', function(){
   beforeEach(module('regexpert'));
 
-  var game;
+  var game, levelText, expectation;
 
   beforeEach(inject(function($controller){
     game = $controller('GameController');
+    levelText = 'Horse brHeeding is reproduction in horses, and particularly the human-directed process of selective breeding of animals, particularly purebred horses of a given breed. Planned matings can be used to produce specifically desired characteristics in domesticated horses. Furthermore, modern breeding management and technologies can increase the rate of conception, a healthy pregnancy, and successful foaling.';
+    expectation = 'HPF';
   }));
+
+  it('has level text', function() {
+    expect(game.levelText).toEqual(levelText);
+  });
+
+  it('has an expectation', function() {
+    expect(game.expectation).toEqual(expectation);
+  });
 
   describe('#isWon',function(){
     it('when input matches expectation returns true',function(){
-      expect(game.isWon('good input')).toEqual(true);
+      expect(game.isWon('\\b[HPF]')).toEqual(true);
     });
 
     it('when input doesnt amtch expectation returnf false',function(){
