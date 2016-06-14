0314d0bb7ade72077e137293835c523b4438e28e
gerauf <fo4786@gmail.com>
2016-06-14 17:03:49 +0100

    creates object type method thing????:hankey:

--- app/js/services/HighlightService.js
+++ app/js/services/HighlightService.js
@@ -2,7 +2,8 @@ angular.module('regexpert')
        .service('HighlightService', ['$sce', function($sce) {
 
   this.highlight = highlight;
-  this.doubleHighlight = doubleHighlight;
+  this.splitIntoComponents = splitIntoComponents;
+  // this.doubleHighlight = doubleHighlight;
 
   function highlight(text, searchString) {
     if (searchString){
@@ -13,26 +14,45 @@ angular.module('regexpert')
     return _convertToHtml(text);
   }
 
-  function doubleHighlight(text, target, searchString) {
+  function getLetterMatchType(text, target, type) {
+    var regex = _makeRegexp(target);
+    var resultArray = [];
+    var matches = text.match(regex);
 
-    firstHighlight = (text.replace(_makeRegexp(target), function(match){
-      return "<span class='highlight t'>" + match + "</span>";
-    }));
 
-    if(searchString){
-      return _convertToHtml(firstHighlight.replace(_makeRegexp(searchString), function(match){
-        return "<span class='highlight s'>" + match + "</span>";
-      }));
-    }
-    
-    return _convertToHtml(firstHighlight);
+    _splitAroundMatches(text,regex).forEach(function(section){
+      if(matches.includes(section)){
+        section.split('').forEach(function(el){
+          resultArray.push({ type: type, text: el});
+        });
+      }else{
+        section.split('').forEach(function(el){
+          resultArray.push({type: 'n',text: el});
+        });
+      }
+    });
+    return resultArray;
   }
+
+  function compareLetterMatchType (obj1, obj2){
+    
+  }
+
 
   function _makeRegexp(input) {
     return new RegExp(input, 'g');
   }
 
+
   function _convertToHtml(text) {
     return $sce.trustAsHtml(text);
   }
+
+
+  function _splitAroundMatches(text,regex){
+    return text.replace(regex,function(match){
+      return '∞' + match + '∞';
+    }).split('∞');
+  }
+
 }]);
--- test/unit/HighlightService.spec.js
+++ test/unit/HighlightService.spec.js
@@ -1,7 +1,7 @@
 describe('HighlightService', function() {
   beforeEach(module('regexpert'));
 
-  var sce, HighlightService, text, target;
+  var sce, HighlightService, text, target, output;
 
   beforeEach(inject(function($sce, _HighlightService_){
     sce = $sce;
@@ -28,20 +28,71 @@ describe('HighlightService', function() {
   });
 
   describe('#doubleHighlight', function() {
+    // beforeEach(function(){
+    //   text = "Some string";
+    //   target = "me";
+    //   searchString = 'g';
+    // });
+
+    // it('highlights the target', function() {
+    //   output = sce.trustAsHtml("So<span class='highlight t'>me</span> string");
+    //   expect(HighlightService.doubleHighlight(text, target).toString()).toEqual(output.toString());
+    // });
+    //
+    // it('highlights the searchString', function() {
+    //   output = sce.trustAsHtml("So<span class='highlight t'>me</span> strin<span class='highlight s'>g</span>");
+    //   expect(HighlightService.doubleHighlight(text, target, searchString).toString()).toEqual(output.toString());
+    // });
+
+
+  });
+
+  describe('#getLetterMatchType', function(){
     beforeEach(function(){
-      text = "Some string";
-      target = "me";
-      searchString = 'g';
+      text = "abcb";
+      target = "b";
     });
 
-    it('highlights the target', function() {
-      output = sce.trustAsHtml("So<span class='highlight t'>me</span> string");
-      expect(HighlightService.doubleHighlight(text, target).toString()).toEqual(output.toString());
+    it('assigns each letter to an object which has a highlight match type', function(){
+      output = [
+        {text: 'a', type: 'n'},
+        {text: 'b', type: 't'},
+        {text: 'c', type: 'n'},
+        {text: 'b', type: 't'}
+      ];
+      expect(HighlightService.splitIntoComponents(text, target, 't')).toEqual(output);
     });
 
-    it('highlights the searchString', function() {
-      output = sce.trustAsHtml("So<span class='highlight t'>me</span> strin<span class='highlight s'>g</span>");
-      expect(HighlightService.doubleHighlight(text, target, searchString).toString()).toEqual(output.toString());
+    it('highlight type decided at input', function(){
+      output = [
+        {text: 'a', type: 'n'},
+        {text: 'b', type: 'p'},
+        {text: 'c', type: 'n'},
+        {text: 'b', type: 'p'}
+      ];
+      expect(HighlightService.splitIntoComponents(text, target, 'p')).toEqual(output);
     });
+  });
+
+  describe('#compareLetterMatchType',function(){
+    beforeEach(function(){
+      text = "abcb";
+      targetOne = "b";
+      targetTwo = "cb";
+      s = HighlightService.splitIntoComponents(text, targetOne, 's');
+      t = HighlightService.splitIntoComponents(text, targetTwo, 't');
+      var zipped = compareLetterMatchType(s,t);
+    });
+
+    it('if both types n then n', function(){
+      expect(zipped[0].type).toEqaul('n');
+    });
+    it('if n and something else then something else', function(){
+      expect(zipped[1].type).toEqaul('s');
+    });
+    it('if both something else then show both', function(){
+      expect(zipped[3].type).toEqaul('st');
+    });
+
   });
 });
