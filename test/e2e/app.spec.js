83b1edde3863ccef5dd38eab8ca772984f914e86
Emmett Walsh <emmett.finbarr.walsh@gmail.com>
2016-06-15 18:45:12 +0100

    Refactors feature tests to reduce run time

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -6,8 +6,7 @@ describe("regexpert", function(){
     browser.get('/');
     levelText = 'Horse brHeeding is reproduction in horses, and particularly the human-directed process of selective breeding of animals, particularly purebred horses of a given breed. Planned matings can be used to produce specifically desired characteristics in domesticated horses. Furthermore, modern breeding management and technologies can increase the rate of conception, a healthy pregnancy, and successful foaling.';
     levelText2 = 'Some kind of nonsense';
-    expectation = 'HPF';
-  });
+    });
 
   it('highlights text based on input', function(){
     $('input#user-input').sendKeys("[a-p]");
@@ -17,32 +16,20 @@ describe("regexpert", function(){
     expect($('p#level-text').$$('span.target.search').first().getText()).toEqual('i');
   });
 
-  it('a game can be won', function(){
+  it('a level can be beaten', function(){
     expect($('span#win-message').getText()).not.toMatch('WINNER');
     $('input#user-input').sendKeys('\\b[a-z]..\\b');
     expect($('span#win-message').getText()).toEqual('WINNER');
-  });
-
-  it('a next level button appears when the game is won', function(){
-    $('input#user-input').sendKeys("\\b[a-z]..\\b");
     expect($('button#next-level').isPresent()).toBe(true);
+    $('button#next-level').click();
+    expect($('section#level-number').getText()).toEqual('Level: 2');
+    expect($('p#level-text').getText()).toEqual(levelText2);
+    expect($('button#next-level').isPresent()).toBe(false);
   });
 
   it('starts on level 1', function() {
     expect($('p#level-text').getText()).toEqual(levelText);
   });
 
-  it('loads level 2', function() {
-    $('input#user-input').sendKeys("\\b[a-z]..\\b");
-    $('button#next-level').click();
-    expect($('section#level-number').getText()).toEqual('Level: 2');
-    expect($('p#level-text').getText()).toEqual(levelText2);
-  });
-
-  it('removes the next level button on click', function() {
-    $('input#user-input').sendKeys("\\b[a-z]..\\b");
-    $('button#next-level').click();
-    expect($('button#next-level').isPresent()).toBe(false);
-  });
 
 });
