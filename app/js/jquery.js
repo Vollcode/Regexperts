a041413ac2ae659592514e3d99dc92f78a741dbd
gerauf <fo4786@gmail.com>
2016-06-20 13:25:21 +0100

    adds cheatsheet as a modal

--- app/js/jquery.js
+++ app/js/jquery.js
@@ -1,26 +1,32 @@
-setInterval(moveSnowden, 100);
-var keys = {};
-var switcher;
+$(document).ready(function(){
 
-$(document).keydown(function(e) {
-    keys[e.keyCode] = true;
-});
+  setInterval(moveSnowden, 100);
+  var keys = {};
+  var switcher;
 
-$(document).keyup(function(e) {
-    delete keys[e.keyCode];
-});
+  $(document).keydown(function(e) {
+      keys[e.keyCode] = true;
+  });
+
+  $(document).keyup(function(e) {
+      delete keys[e.keyCode];
+  });
+
+  $('.modal-trigger').leanModal();
 
 
-function moveSnowden() {
-  for (var key in keys) {
-    if(keys[key] && switcher){
-      $("#left-hand").animate({top: "-=50"}, 0);
-      $("#right-hand").animate({top: "+=50"}, 0);
-      switcher = false;
-    } else {
-      $("#left-hand").animate({top: "+=50"}, 0);
-      $("#right-hand").animate({top: "-=50"}, 0);
-      switcher = true;
+  function moveSnowden() {
+    for (var key in keys) {
+      if(keys[key] && switcher){
+        $("#left-hand").animate({top: "-=50"}, 0);
+        $("#right-hand").animate({top: "+=50"}, 0);
+        switcher = false;
+      } else {
+        $("#left-hand").animate({top: "+=50"}, 0);
+        $("#right-hand").animate({top: "-=50"}, 0);
+        switcher = true;
+      }
     }
   }
-}
+
+});
