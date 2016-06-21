7a46be7ba8c77c9303c9bc904f06f30bc731341c
emilio <emsmnndz@gmail.com>
2016-06-21 11:07:17 +0100

    sets, gets and resets checkpoints, and restes score

--- app/js/services/GameService.js
+++ app/js/services/GameService.js
@@ -5,21 +5,46 @@ function GameService(){
   var self = this;
 
   this.score = 0;
+  this.checkpoint =1;
   this.getScore = getScore;
-  this.updateScore = updateScore;
   this.setScore = setScore;
+  this.resetScore = resetScore;
+  this.getCheckpoint = getCheckpoint;
+  this.setCheckpoint = setCheckpoint;
+  this.resetCheckpoint = resetCheckpoint;
 
-  function updateScore(amount) {
+  function setScore(amount) {
     this.score += amount;
-    self.setScore(this.score);
+    saveToStorage('currentScore', this.score);
   }
 
   function getScore() {
     return JSON.parse(localStorage.getItem('currentScore'));
   }
 
-  function setScore(score) {
-    localStorage.setItem('currentScore', JSON.stringify(score));
+  function getCheckpoint() {
+    return JSON.parse(localStorage.getItem('checkpoint'));
   }
 
+  function setCheckpoint(levelNumber) {
+    if (levelNumber % 3 === 0) {
+      this.checkpoint = levelNumber;
+      saveToStorage('checkpoint',this.checkpoint);
+    }
+  }
+
+  function resetScore() {
+    this.score = 0;
+    saveToStorage('currentScore', 0);
+  }
+
+  function resetCheckpoint() {
+    this.checkpoint = 1;
+    saveToStorage('checkpoint',1);
+
+  }
+
+  function saveToStorage(item,value) {
+    localStorage.setItem(item, JSON.stringify(value));
+  }
 }
